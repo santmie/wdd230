63712ce8c48d760df93e09fed10eaf504d1ee85f
santmie <san.advincula@gmail.com>
2024-04-01 17:42:31 +0800

    wk5- update team activity

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,23 +9,31 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); //testing only
-            displayResults(data); //uncomment when ready
+            console.log(data); // testing only
+            displayResults(data); // uncomment when ready
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error);
-    }     
+    }
 }
 
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;C`; // Display temperature in Fahrenheit
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`; // Display temperature in Celsius
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; // Fill in the blanks for icon source
     let desc = data.weather[0].description; // Fill in the blank for weather description
-    weatherIcon.setAttribute('src', iconsrc); // Set attribute for src
+    
+    // Ensure that the iconsrc is not empty before setting the src attribute
+    if (iconsrc) {
+        weatherIcon.setAttribute('src', iconsrc); // Set attribute for src
+    } else {
+        // Provide a fallback image source if iconsrc is empty
+        weatherIcon.setAttribute('src', 'fallback-icon.png');
+    }
+
     weatherIcon.setAttribute('alt', desc); // Set attribute for alt
     captionDesc.textContent = `${desc}`; // Fill in the blank for caption description
 }
